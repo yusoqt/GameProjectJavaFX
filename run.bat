7611a8ca1ae259810aaa33cb27f7d7b9f2c31924
Kritchaya chaowajareun <6733007821@student.chula.ac.th>
2025-03-01 20:48:44 +0700

    Initial commit: Add sequence diagrams documentation

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Running JavaFX application...
+java --module-path "C:\Program Files\Java\javafx-sdk-23.0.2\lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.media -cp bin application.MainApp
